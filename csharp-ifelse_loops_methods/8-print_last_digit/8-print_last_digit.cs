58e9dd17f239707c11e01cc8d8e27e7bb3f20b15
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-01 13:54:29 -0400

    class to print the last digit of a number

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,10 @@
+﻿class Number {
+    public static int PrintLastDigit(int number) {
+        int lastNum = Math.Abs(number) % 10;
+        if (lastNum < 0) {
+            lastNum *= -1;
+        }
+        Console.Write("{0}", lastNum);
+        return lastNum;
+    }
+}

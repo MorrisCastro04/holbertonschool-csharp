c33eb3f020e769635063824e5bf3439414df9391
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-01 12:16:53 -0400

    program to print comma-separated numbers from 0 to 99

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        for (int num = 0; num <= 99; num++) {
+            if (num == 99) {
+                Console.Write("{0}\n", num);
+            }else {
+                Console.Write("{0}, ", num.ToString("00"));
+            }
+        }
+    }
+}

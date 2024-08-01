984811ae5aa2e7798d11018f3ebc016a36ee1173
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-01 12:09:56 -0400

    convert form decimal to hexa

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int number = 0; number <= 98; number++) {
+            Console.Write("{0} = 0x{1}\n", number, number.ToString("x"));
+        }
+    }
+}

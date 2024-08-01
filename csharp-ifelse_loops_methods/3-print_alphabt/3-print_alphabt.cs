71dbc26950f61c4815e006938cf348215df32487
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-01 11:34:30 -0400

    program to print the alphabet exept e and q in C#

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            if (letter == 'e' || letter == 'q') {
+                continue;
+            } else {
+                Console.Write("{0}", letter);
+            }
+        }
+    }
+}

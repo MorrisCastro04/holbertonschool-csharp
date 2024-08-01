872f5828ea69d4790ac69acba19892baa4c9b60c
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-01 14:04:33 -0400

    script to draw a line

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,10 @@
+﻿class Line {
+    public static void PrintLine(int length) {
+        if (length > 0) {
+            for (int line = 0; line < length; line++) {
+                Console.Write("_");
+            }
+        }
+        Console.Write("\n");
+    }
+}

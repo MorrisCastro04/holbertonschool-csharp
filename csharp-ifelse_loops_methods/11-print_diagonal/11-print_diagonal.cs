d0bb58c163ff1e7e22b8390c554500a1285c0801
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-01 14:15:39 -0400

    class to print a diagonal line

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,13 @@
+﻿class Line {
+    public static void PrintDiagonal(int length) {
+        if (length > 0) {
+            for (int line = 1; line <= length; line++) {
+                for (int space = 1; space < line; space++){
+                    Console.Write(" ");
+                }
+                Console.Write("\\\n");
+            }
+        }
+        Console.Write("\n");
+    }
+}

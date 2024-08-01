ef56512fd7fe52e07027ce50d6326b3b62c49194
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-01 13:42:59 -0400

    IsLower method to check if a character is lowercase

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,10 @@
+﻿class Character {
+    public static bool IsLower(char c) {
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            if (letter == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

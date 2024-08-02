053bfc2ed49635999c41a61a2ebf9e5d6e381d17
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-02 14:32:15 -0400

    class to print an array

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,28 @@
+﻿class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        if (size == 0)
+        {
+            Console.WriteLine("");
+        }
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = i;
+            if (array.Length - 1 == i)
+            {
+                Console.Write("{0}\n", array[i]);
+            }
+            else {
+                Console.Write("{0} ", array[i]);
+            }
+        }
+        return array;
+    }
+}

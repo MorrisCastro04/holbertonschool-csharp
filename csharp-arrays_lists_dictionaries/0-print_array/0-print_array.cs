bc7f8da8499aec0453f4bd2c0213c9830262a92d
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-02 15:45:07 -0400

    use string.Join for better readability

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -7,22 +7,12 @@
             Console.WriteLine("Size cannot be negative");
             return null;
         }
-        if (size == 0)
-        {
-            Console.WriteLine("");
-        }
         int[] array = new int[size];
         for (int i = 0; i < size; i++)
         {
             array[i] = i;
-            if (array.Length - 1 == i)
-            {
-                Console.Write("{0}\n", array[i]);
-            }
-            else {
-                Console.Write("{0} ", array[i]);
-            }
         }
+        Console.WriteLine(string.Join(" ", array));
         return array;
     }
 }

275b3750a4da9f5fc0a92348ecb35a7c5d37b2b9
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-04 15:38:59 -0400

    code for printing a jagged array in C#

---
+++ csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jagged = new int[][]
+            {
+                new int[] {0, 1, 2, 3},
+                new int[] {0, 1, 2, 3, 4, 5, 6},
+                new int[] {0, 1}
+            };
+            foreach (int[] col in jagged)
+            {
+                foreach (int row in col)
+                {
+                    Console.Write("{0}", row);
+                    if (row != col[col.Length - 1])
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

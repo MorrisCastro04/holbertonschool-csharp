dab849c62e07b2ad506155e6292a2e0c35b0dede
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-04 01:36:27 -0400

    print 5x5 array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = {{0,0,0,0,0},{0,0,0,0,0},{0,0,1,0,0},{0,0,0,0,0},{0,0,0,0,0}};
+
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    Console.Write("{0} ", array[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

a09763c7789a5cea8fe0f0d6295a32c8d28b9c0a
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-03 21:12:19 -0400

    delete the namespace

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,30 +1,27 @@
 ﻿using System;
 
-namespace _0_print_array
+class Array
 {
-    class Array
+    public static int[] CreatePrint(int size)
     {
-        public static int[] CreatePrint(int size)
+        if (size < 0)
         {
-            if (size < 0)
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
+        }
+        else
+        {
+            int[] newArray = new int[size];
+            for (int i = 0; i < size; i++)
             {
-                Console.WriteLine("Size cannot be negative");
-                return null;
+                newArray[i] = i;
             }
-            else if (size == 0)
-            {
-                Console.WriteLine();
-                return new int[0];
-            }
-            else
-            {
-                int[] newArray = new int[size];
-                for (int i = 0; i < size; i++)
-                {
-                    newArray[i] = i;
-                }
-                return newArray;
-            }
+            return newArray;
         }
     }
 }

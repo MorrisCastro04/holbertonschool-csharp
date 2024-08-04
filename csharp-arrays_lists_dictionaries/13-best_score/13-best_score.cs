a0104a4888c67f867999419327a883ab6ba87fa9
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-04 01:12:47 -0400

    search the beast score

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myDict)
+    {
+        string bestKey = "None";
+        int bestValue = 0;
+
+        foreach (var kvp in myDict)
+        {
+            if (kvp.Value > bestValue)
+            {
+                bestKey = kvp.Key;
+                bestValue = kvp.Value;
+            }
+        }
+
+        return bestKey;
+    }
+}

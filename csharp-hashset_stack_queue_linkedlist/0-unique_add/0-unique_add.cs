bd9d9083efc73354ebcf1e63a459ff6376c28b0a
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-05 10:24:06 -0400

    sum all uniques nums

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        HashSet<int> myHash = new HashSet<int>(myList);
+
+        foreach (int num in myHash)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+}

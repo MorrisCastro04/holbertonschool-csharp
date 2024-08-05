75217f8b0875603785ca64ac7266fb81ff4dd148
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-05 16:39:21 -0400

    count the nodes in a LinkedList

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int num = 0;
+        foreach (int nodes in myLList)
+        {
+            num++;
+        }
+        return num;
+    }
+}

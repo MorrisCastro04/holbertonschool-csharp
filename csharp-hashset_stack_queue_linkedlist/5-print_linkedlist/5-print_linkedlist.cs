b7d42593687264def34d3c6e40f7d645c1418d9d
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-05 16:33:53 -0400

    add and print the data of a linkedList

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        if (size < 0)
+            return new LinkedList<int>();
+
+        LinkedList<int> link = new LinkedList<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            link.AddFirst(i);
+            Console.WriteLine(i);
+        }
+        return link;
+    }
+}

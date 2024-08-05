e4042cb1a326ce5411f724c8835a28fce082c0f4
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-05 17:09:12 -0400

    if the link list is empty return 0

--- csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -5,6 +5,9 @@ class LList
 {
     public static int Pop(LinkedList<int> myLList)
     {
+        if (myLList.Count == 0)
+            return 0;
+
         int lastValue = myLList.First.Value;
         myLList.RemoveFirst();
         return lastValue;

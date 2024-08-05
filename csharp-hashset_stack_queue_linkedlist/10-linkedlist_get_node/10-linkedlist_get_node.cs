a4ab8a6971ceea368fe1fb89cb054a17c5fcc036
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-05 17:54:57 -0400

    get a node

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> currentNode = myLList.First;
+        for (int i = 0; i < myLList.Count; i++)
+        {
+            if (n == i)
+            {
+                return currentNode.Value;
+            }
+            currentNode = currentNode.Next;
+        }
+        return 0;
+    }
+}

2b82e746e50eefae31742d74e162a11295de4f6f
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-05 17:01:41 -0400

    look for a value in the link list

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int count = 0;
+        foreach (int num in myLList)
+        {
+            if (value == num)
+                return count;
+            count++;
+        }
+        return -1;
+    }
+}

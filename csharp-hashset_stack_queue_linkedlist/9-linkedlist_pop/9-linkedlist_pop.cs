77e35e3ed5ba5742117944dd5ec584f4fa80bdc0
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-05 17:08:26 -0400

    remove first value

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int lastValue = myLList.First.Value;
+        myLList.RemoveFirst();
+        return lastValue;
+    }
+}

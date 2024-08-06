9f230fb6302d7ba5cddf12c2aacec2b59561ce3e
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-06 10:31:46 -0400

    insert a node in a link list sorted

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+
+        foreach (int num in myLList)
+        {
+            if (num > n)
+            {
+                myLList.AddBefore(current, n);
+                return current;
+            }
+            current = current.Next;
+        }
+        return current;
+    }
+}

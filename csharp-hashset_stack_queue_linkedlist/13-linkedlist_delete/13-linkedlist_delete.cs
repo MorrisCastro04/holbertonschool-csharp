d02cd2d1b94544dae3c69a35175ea5d84b05a554
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-08-08 16:56:31 -0400

    delete equeal in the first if

--- csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -5,7 +5,7 @@ class LList
 {
     public static void Delete(LinkedList<int> myLList, int index)
     {
-        if (index < 0 || index >= myLList.Count)
+        if (index < 0 || index > myLList.Count)
             return;
 
         LinkedListNode<int> current = myLList.First;

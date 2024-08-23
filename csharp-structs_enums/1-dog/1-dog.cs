62d9a81fe699a8e63722681958009968bab89a59
Morris Jomar <jomarcastro2004@gmail.com>
2024-08-23 14:01:36 -0400

    change indent

--- csharp-structs_enums/1-dog/1-dog.cs
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -2,15 +2,13 @@
 
 public enum Rating
 {
-    Good,
-    Great,
-    Excellent
+  Good, Great, Excellent
 }
 
 public struct Dog
 {
-    public string name;
-    public float age;
-    public string owner;
-    public Rating rating;
+  public string name;
+  public float age;
+  public string owner;
+  public Rating rating;
 }

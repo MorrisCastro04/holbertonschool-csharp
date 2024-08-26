97f96db98f8f5640194a9d2323eb828ede5a0194
Morris Jomar <jomarcastro2004@gmail.com>
2024-08-26 13:10:35 -0400

    override toString

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie class.
+    /// </summary>
+    public class Zombie
+    {
+        // health: Represents the health of the zombie
+        // name: Represents the zombie name
+        private int health;
+        private string name = "(No name)";
+
+        /// <summary>
+        /// Gets or sets the name of the zombie.
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// Sets the zombie health.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Sets the zombie health.
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                health = value;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+
+        /// <summary>
+        /// Returns the value of Health.
+        ///</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        ///<summary>
+        ///Return the information of the zombie
+        ///</summary>
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+    }
+}

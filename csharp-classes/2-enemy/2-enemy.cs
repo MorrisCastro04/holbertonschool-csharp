cc003838f2312f1becaa81e062d68b46886ae417
Morris Jomar <jomarcastro2004@gmail.com>
2024-08-26 11:22:36 -0400

    add constructure to get a health value

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie class.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represents the health of the zombie
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// set the zombie health
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// set the zombie health
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                health = value;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+    }
+}

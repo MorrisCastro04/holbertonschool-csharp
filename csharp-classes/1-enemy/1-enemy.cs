dedad593593dea29cf8c007ffd999f4e0460b117
Morris Jomar <jomarcastro2004@gmail.com>
2024-08-26 10:59:02 -0400

    add constructor

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie class.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represents the health of the zombie
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// set the zombie health
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}

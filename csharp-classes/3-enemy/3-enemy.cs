47cbe94169329dd2fb89375c41b7be5852ee03bc
Morris Jomar <jomarcastro2004@gmail.com>
2024-08-26 11:31:09 -0400

    method to return the health

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie class.
+    /// </summary>
+    public class Zombie
+    {
+        // Represents the health of the zombie
+        private int health;
+
+        /// <summary>
+        /// set the zombie health
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// set the zombie health
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                health = value;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+
+        /// <summary>
+        ///return the value of Health
+        ///</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}

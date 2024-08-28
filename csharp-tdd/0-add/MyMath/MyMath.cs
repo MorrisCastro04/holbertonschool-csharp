ae4161ef0c86bb7ba23602c123365494d2120533
Morris Jomar <jomarcastro2004@gmail.com>
2024-08-28 13:32:01 -0400

    add doc to the params and return

--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -8,6 +8,9 @@ public class Operations
     ///<summary>
     ///method the sum 2 numbers
     ///</summary>
+    /// <param name="a">first value</param>
+    /// <param name="b">second value</param>
+    /// <returns>sum of parameters</returns>
     public static int Add(int a, int b){
         return a + b;
     }

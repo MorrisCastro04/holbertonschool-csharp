38326f9ee2cffb6dcc30ea56849e76bcfbbe29b5
Morris Jomar <jomarcastro2004@gmail.com>
2024-08-28 13:22:44 -0400

    test a sum method

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,14 @@
+﻿namespace MyMath;
+
+///<summary>
+///Operation class
+///</summary>
+public class Operations
+{
+    ///<summary>
+    ///method the sum 2 numbers
+    ///</summary>
+    public static int Add(int a, int b){
+        return a + b;
+    }
+}

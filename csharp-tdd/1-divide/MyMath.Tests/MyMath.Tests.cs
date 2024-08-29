a77b5bd71e3930e00f54bf81ad90af0cfd86b131
Morris Jomar <jomarcastro2004@gmail.com>
2024-08-29 09:50:59 -0400

    add test to check console

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -1,5 +1,7 @@
 using MyMath;
+using System;
 using NUnit.Framework;
+using System.IO; 
 
 namespace MyMath.Tests
 {
@@ -14,14 +16,24 @@ namespace MyMath.Tests
             int[,] result = Matrix.Divide(matrix, num);
             Assert.That(result, Is.EqualTo(new int[,] { { 0, 1, 1 }, { 2, 2, 3 }, { 3, 4, 4 } }));
         }
-        [Test]
-        public void TestDivideByZero()
+        [TestCase(0)]
+        public void TestDivideByZero(int divisor)
         {
             int[,] matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-            int num = 0;
-            int[,] result = Matrix.Divide(matrix, num);
+            int[,] result = Matrix.Divide(matrix, divisor);
             Assert.That(result, Is.Null);
         }
+        [TestCase(0)]
+public void TestDivideByZeroConsole(int divisor)
+{
+    StringWriter stringWriter = new StringWriter();
+    Console.SetOut(stringWriter);
+
+    int[,] matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+    int[,] result = Matrix.Divide(matrix, divisor);
+
+    Assert.That(stringWriter.ToString().Trim(), Is.EqualTo("Num cannot be 0"));
+}
         [Test]
         public void TestDivideNull()
         {

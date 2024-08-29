b5133669bc8698c05c1cc7d1f03b88ea020986e8
Morris Jomar <jomarcastro2004@gmail.com>
2024-08-29 15:56:06 -0400

    chore: Refactor Matrix.Divide method and tests

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -1,80 +1,96 @@
-using MyMath;
-using System;
 using NUnit.Framework;
-using System.IO; 
+using System.Linq;
+using System;
+using System.IO;
 
 namespace MyMath.Tests
 {
     [TestFixture]
-    public class Tests
+    public class MatrixTests
     {
-        [Test]
-        public void TestDivide()
+        private int[,] matrix;
+
+        [SetUp]
+        public void SetUp()
         {
-            int[,] matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-            int num = 2;
-            int[,] result = Matrix.Divide(matrix, num);
-            Assert.That(result, Is.EqualTo(new int[,] { { 0, 1, 1 }, { 2, 2, 3 }, { 3, 4, 4 } }));
+            // Initialize test matrix
+            this.matrix = new int[,] {
+                { 0, 1, -2, 99, 100000 },
+                { 13, -4, 566, -77777, 88 },
+                { 10049, -666, 53, -1, 9999999 },
+            };
         }
+
+        [Test]
+        public void Divide_ByNonZeroIntegers_ReturnsCorrectQuotients(
+            [Values(-99, 100, 4, 40000, -66, 631, 97531)] int divisor)
+        {
+
+            // Arrange - create expected matrix
+            int[,] expectedMatrix = new int[5,5];
+
+            for (var y = 0; y < matrix.GetLength(0); y++)
+            {
+                for (var x = 0; x < matrix.GetLength(1); x++)
+                {
+                    expectedMatrix[y,x] = matrix[y,x] / divisor;
+                }
+            }
+
+            // Act - divide test matrix and check against expected output
+            var dividedMatrix = Matrix.Divide(this.matrix, divisor);
+
+            bool matricesAreEqual = true;
+
+            for (var y = 0; y < matrix.GetLength(0); y++)
+            {
+                for (var x = 0; x < matrix.GetLength(1); x++)
+                {
+                    // Check if items at index each match
+                    if (matrix[y,x] != expectedMatrix[y,x])
+                    {
+                        matricesAreEqual = false;
+                        break;
+                    }
+                }
+            }
+
+            // Assert
+            Assert.That(matricesAreEqual, Is.True);
+        }
+
         [TestCase(0)]
-        public void TestDivideByZero(int divisor)
+        public void Divide_ByZero_ReturnsNull(int divisor)
         {
-            int[,] matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-            int[,] result = Matrix.Divide(matrix, divisor);
-            Assert.That(result, Is.Null);
+            // Act - create test array with class method
+            var dividedMatrix = Matrix.Divide(this.matrix, divisor);
+
+            // Assert
+            Assert.That(dividedMatrix == null);
         }
+
         [TestCase(0)]
-public void TestDivideByZeroConsole(int divisor)
-{
-    StringWriter stringWriter = new StringWriter();
-    Console.SetOut(stringWriter);
+        public void Divide_ByZero_PrintsToConsole(int divisor)
+        {
+            // Set output stream
+            StringWriter stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
 
-    int[,] matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-    int[,] result = Matrix.Divide(matrix, divisor);
+            // Act - create test array with class method
+            var dividedMatrix = Matrix.Divide(this.matrix, divisor);
 
-    Assert.That(stringWriter.ToString().Trim(), Is.EqualTo("Num cannot be 0"));
-}
-        [Test]
-        public void TestDivideNull()
-        {
-            int[,] matrix = null;
-            int num = 2;
-            int[,] result = Matrix.Divide(matrix, num);
-            Assert.That(result == null);
-        }
-        [Test]
-        public void TestDivideEmpty()
-        {
-            int[,] matrix = new int[,] { };
-            int num = 2;
-            int[,] result = Matrix.Divide(matrix, num);
-            Assert.That(result, Is.EqualTo(new int[,] { }));
-        }
-        [Test]
-        public void TestDivideNegative()
-        {
-            int[,] matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-            int num = -2;
-            int[,] result = Matrix.Divide(matrix, num);
-            Assert.That(result, Is.EqualTo(new int[,] { { 0, -1, -1 }, { -2, -2, -3 }, { -3, -4, -4 } }));
+            // Assert
+            Assert.That(stringWriter.ToString().Trim() == "Num cannot be 0\n");
         }
 
         [Test]
-        public void TestDivideMatrixWithZeroElements()
+        public void Divide_MatrixIsNull_ReturnsNull()
         {
-            int[,] matrix = new int[,] { { 0, 2, 0 }, { 4, 0, 6 }, { 0, 8, 0 } };
-            int num = 2;
-            int[,] result = Matrix.Divide(matrix, num);
-            Assert.That(result, Is.EqualTo(new int[,] { { 0, 1, 0 }, { 2, 0, 3 }, { 0, 4, 0 } }));
-        }
+            // Act - create test array with class method
+            var dividedMatrix = Matrix.Divide(null, 9);
 
-        [Test]
-        public void TestDivideSingleElementMatrix()
-        {
-            int[,] matrix = new int[,] { { 10 } };
-            int num = 2;
-            int[,] result = Matrix.Divide(matrix, num);
-            Assert.That(result, Is.EqualTo(new int[,] { { 5 } }));
+            // Assert
+            Assert.That(dividedMatrix == null);
         }
     }
 }
--- csharp-tdd/1-divide/MyMath/MyMath.cs
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -1,40 +1,28 @@
-﻿using System;
+﻿﻿using System;
 
 namespace MyMath
 {
-    ///<summary>
-    ///class Matrix
-    /// </summary>
-    public class Matrix
+  public class Matrix
+  {
+    public static int[,] Divide(int[,] matrix, int num)
     {
-        ///<summary>
-        ///Divide method
-        ///</summary>
-        ///<param name="matrix">matrix</param>
-        ///<param name="num">num</param>
-        ///<returns>return a matrix</returns>
-        public static int[,] Divide(int[,] matrix, int num)
+      try{
+        for(int i = 0; i < matrix.GetLength(0); i++)
         {
-            try
-            {
-                for (int i = 0; i < matrix.GetLength(0); i++)
-                {
-                    for (int j = 0; j < matrix.GetLength(1); j++)
-                    {
-                        matrix[i, j] = matrix[i, j] / num;
-                    }
-                }
-                return matrix;
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Num cannot be 0");
-                return null;
-            }
-            catch (NullReferenceException)
-            {
-                return null;
-            }
+          for(int j = 0; j < matrix.GetLength(1); j++)
+          {
+            matrix[i, j] = matrix[i, j] / num;
+          }
         }
+        return matrix;
+      }catch(DivideByZeroException)
+      {
+        Console.WriteLine("Num cannot be 0");
+        return null;
+      }catch(NullReferenceException)
+      {
+        return null;
+      }
     }
+  } 
 }

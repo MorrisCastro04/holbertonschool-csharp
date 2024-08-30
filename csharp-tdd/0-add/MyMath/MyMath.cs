1e67e40ab9cc4ba92233ba88f0cc44602aa27a24
Morris Jomar <jomarcastro2004@gmail.com>
2024-08-30 09:37:55 -0400

    make task 0 again

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyMath
+{
+    ///<summary>
+    ///Operation class
+    ///</summary>
+    public class Operations
+    {
+        ///<summary>
+        ///method the sum 2 numbers
+        ///</summary>
+        /// <param name="a">first value</param>
+        /// <param name="b">second value</param>
+        /// <returns>sum of parameters</returns>
+        public static int Add(int a, int b){
+            return a + b;
+        }
+    }
+}

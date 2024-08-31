e7a21911b396b2001acc09c1a7c1b7ce31e0a866
Morris Jomar <jomarcastro2004@gmail.com>
2024-08-31 16:22:05 -0400

    Task 5

---
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Text
+{
+    public class Str
+    {
+        public static int CamelCase(string s)
+        {
+            if (s == null || s == "")
+            {
+                return -1;
+            }
+            int count = 1;
+            foreach (char c in s)
+            {
+                if (char.IsUpper(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}

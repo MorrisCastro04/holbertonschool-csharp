50b779967859d78d490d724c9786ebeb3d9d334a
Morris Jomar <jomarcastro2004@gmail.com>
2024-08-31 16:24:19 -0400

    add odc

--- csharp-tdd/5-camelcase/Text/Text.cs
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -2,8 +2,16 @@
 
 namespace Text
 {
+    /// <summary>
+    /// Class to count the number of words in a camel case string.
+    /// </summary>
     public class Str
     {
+        /// <summary>
+        /// Count the number of words in a camel case string.
+        /// </summary>
+        /// <param name="s">The string to count the words in.</param>
+        /// <returns>The number of words in the string.</returns>
         public static int CamelCase(string s)
         {
             if (s == null || s == "")

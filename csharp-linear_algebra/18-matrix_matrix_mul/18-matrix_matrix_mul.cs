9cee7eff29a20c72114714cade2eab99f9d3c154
Morris Jomar <jomarcastro2004@gmail.com>
2024-09-18 15:58:31 -0400

    change if statement

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -13,21 +13,21 @@ class MatrixMath
     /// <returns>The resulting matrix. If the matrices cannot be multiplied, returns a matrix containing -1.</returns>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        if (matrix1.GetLength(0) == matrix2.GetLength(1))
+        if (matrix1.GetLength(0) != matrix2.GetLength(1))
         {
-            double[,] matrix3 = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
-            for (int rows1 = 0; rows1 < matrix1.GetLength(0); rows1++)
+            return new double[,] { { -1 } };
+        }
+        double[,] matrix3 = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+        for (int rows1 = 0; rows1 < matrix1.GetLength(0); rows1++)
+        {
+            for (int col2 = 0; col2 < matrix2.GetLength(1); col2++)
             {
-                for (int col2 = 0; col2 < matrix2.GetLength(1); col2++)
+                for (int i = 0; i < matrix1.GetLength(1); i++)
                 {
-                    for (int i = 0; i < matrix1.GetLength(1); i++)
-                    {
-                        matrix3[rows1, col2] += matrix1[rows1, i] * matrix2[i, col2];
-                    }
+                    matrix3[rows1, col2] += matrix1[rows1, i] * matrix2[i, col2];
                 }
             }
-            return matrix3;
         }
-        return new double[,] { { -1 } };
+        return matrix3;
     }
 }

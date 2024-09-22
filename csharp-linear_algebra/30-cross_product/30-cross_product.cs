1b7855e3bb464893fa230ccf35aeccb6088d8929
Morris Jomar <jomarcastro2004@gmail.com>
2024-09-21 23:16:45 -0400

    round letters

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -19,9 +19,9 @@ class VectorMath
         if (vector1.Length != 3 && vector2.Length != 3)
             return new double[] {-1};
 
-        double x = vector1[1] * vector2[2] - vector1[2] * vector2[1];
-        double y = vector1[2] * vector2[0] - vector1[0] * vector2[2];
-        double z = vector1[0] * vector2[1] - vector1[1] * vector2[0];
+        double x = Math.Round(vector1[1] * vector2[2] - vector1[2] * vector2[1], 2);
+        double y = Math.Round(vector1[2] * vector2[0] - vector1[0] * vector2[2], 2);
+        double z = Math.Round(vector1[0] * vector2[1] - vector1[1] * vector2[0], 2);
 
         return new double[] {x,y,z};
     }

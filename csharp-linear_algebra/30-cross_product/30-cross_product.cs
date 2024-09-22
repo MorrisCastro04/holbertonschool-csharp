2993965f9e706bd78bf4113afbc2cc1f4c9942c5
Morris Jomar <jomarcastro2004@gmail.com>
2024-09-21 23:24:14 -0400

    change && to ||

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -16,7 +16,7 @@ class VectorMath
     /// </returns>
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
-        if (vector1.Length != 3 && vector2.Length != 3)
+        if (vector1.Length != 3 || vector2.Length != 3)
             return new double[] { -1 };
 
         double x = Math.Round((vector1[1] * vector2[2]) - (vector1[2] * vector2[1]), 2);

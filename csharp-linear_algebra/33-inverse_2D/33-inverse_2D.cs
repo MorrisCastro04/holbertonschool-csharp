9c8d2de420714f695bb15bea8fa7e859bd624d40
Morris Jomar <jomarcastro2004@gmail.com>
2024-09-22 00:43:07 -0400

    Task 33

---
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,31 @@
+﻿public class MatrixMath
+{
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double a = matrix[0, 0];
+        double b = matrix[0, 1];
+        double c = matrix[1, 0];
+        double d = matrix[1, 1];
+
+        double det = (a * d) - (b * c);
+
+        if (det == 0)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double[,] inverse = new double[2, 2];
+
+        inverse[0, 0] = d / det;
+        inverse[0, 1] = -b / det;
+        inverse[1, 0] = -c / det;
+        inverse[1, 1] = a / det;
+
+        return inverse;
+    }
+}

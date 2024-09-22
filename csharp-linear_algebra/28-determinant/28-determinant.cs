77c506eca9a0857434609568800ef33854ea58e5
Morris Jomar <jomarcastro2004@gmail.com>
2024-09-22 00:06:31 -0400

    add 2D funcion

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -1,30 +1,33 @@
 ﻿using System;
 
 /// <summary>
-/// MatrixMath class
+/// Represents a class of matrix operations.
 /// </summary>
 class MatrixMath
 {
+    public static double Determinant2D(double[,] matrix)
+    {
+        return (Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2));
+    }
     /// <summary>
-    /// Method that calculates the determinant of a matrix
+    /// Represents a method to calculate the determinant of a matrix.
     /// </summary>
-    /// <param name="matrix">Matrix to calculate the determinant</param>
-    /// <returns>The determinant of the matrix</returns>
+    /// <param name="matrix">The matrix to calculate the determinant of.</param>
+    /// <returns>The determinant of the matrix.</returns>
     public static double Determinant(double[,] matrix)
     {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-
         if (matrix.GetLength(1) > 3 || matrix.GetLength(1) != matrix.GetLength(0))
-            return -1;
-
-        if (cols == 2)
-            return Math.Round(matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0], 2);
-
-        double matrixA = matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[1, 2] * matrix[2, 1]);
-        double matrixB = matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[1, 2] * matrix[2, 0]);
-        double matrixC = matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[1, 1] * matrix[2, 0]);
-
-        return Math.Round(matrixA - matrixB + matrixC, 2);
+        {
+            return (-1);
+        }
+        if (matrix.GetLength(1) == 2)
+        {
+            return (Determinant2D(matrix));
+        }
+        double[,] detA = new double[,] { { matrix[1, 1], matrix[1, 2] }, { matrix[2, 1], matrix[2, 2] } };
+        double[,] detB = new double[,] { { matrix[1, 0], matrix[1, 2] }, { matrix[2, 0], matrix[2, 2] } };
+        double[,] detC = new double[,] { { matrix[1, 0], matrix[1, 1] }, { matrix[2, 0], matrix[2, 1] } };
+        double res = ((matrix[0, 0] * Determinant2D(detA)) - (matrix[0, 1] * Determinant2D(detB))) + matrix[0, 2] * Determinant2D(detC);
+        return Math.Round(res, 2);
     }
 }

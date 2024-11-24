48fde815df26b267422f9cd3dc56f5bd7259853b
Morris Jomar <jomarcastro2004@gmail.com>
2024-11-24 18:29:14 -0400

    add class node and enqueue

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+/// <summary>
+/// Queue class
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count;
+    /// <summary>
+    /// return the type of the queue
+    /// </summary>
+    /// <returns>Type</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+    public class Node
+    {
+        T value { get; set; };
+        public node next { get; set; };
+
+        // node Constructor
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+    public void Enqueue(T value)
+    {
+        Node newNode  = new Node(value);
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+    public int Count()
+    {
+        return count;
+    }
+}

ba6a42282b681536fffbc10d04d0046a10626ccb
Morris Jomar <jomarcastro2004@gmail.com>
2024-11-25 14:27:39 -0400

    add door class

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+/// <summary>
+/// Base class
+/// </summary>
+abstract class Base
+{
+    public string name { get; set; }
+
+    /// <summary>
+    /// Override ToString method
+    /// </summary>
+    /// <returns>string</returns>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType()}";
+    }
+}
+
+/// <summary>
+/// IInteractive interface
+/// </summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary>
+/// IBreakable interface
+/// </summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary>
+/// ICollectable interface
+/// </summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class Door : Base, IInteractive
+{
+    public Door(string value = "Door")
+    {
+        this.name = value;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}

6666c2d3576bb465464885d40e7a6a40a3f414e5
Morris Jomar <jomarcastro2004@gmail.com>
2024-11-29 14:46:10 -0400

    add InventoryLibrary project with BaseClass and solution file

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    public class BaseClass
+    {
+        public string id { get; set; }
+        public DateTime date_created { get; set; }
+        public DateTime date_updated { get; set; }
+    }
+}
+
